3198adeae7284f4c4dd6e090e605d59954a14492
641778262 <641778262@qq.com>
2016-05-18 17:32:41 +0800

    Initial commit

---
+++ app/src/main/java/com/chen/imtest/MainApplication.java
@@ -0,0 +1,13 @@
+package com.chen.imtest;
+
+import android.app.Application;
+
+/**
+ * Created by jiahao on 16/5/18.
+ */
+public class MainApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/com/chen/imtest/fragment/GroupFragment.java
@@ -0,0 +1,25 @@
+package com.chen.imtest.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.chen.imtest.R;
+
+/**
+ * Created by jiahao on 16/5/18.
+ * 群组Fragment
+ */
+public class GroupFragment extends BaseFragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.test,container,false);
+        TextView tv = (TextView) view.findViewById(R.id.tv_title);
+        tv.setText("Group");
+        return view;
+    }
+}
